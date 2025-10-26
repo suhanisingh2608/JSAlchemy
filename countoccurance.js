49b85cd1fd76981e9e1c70cabc858f848a5054e9
suhanisingh2608 <suhanisingh1501@gmail.com>
2025-10-26 23:20:50 +0530

    count occurance

---
+++ countoccurance.js
@@ -0,0 +1,15 @@
+function countCharacters(str) {
+    const count = {};
+
+    for (let char of str) {
+       
+        if (count[char]) {
+            count[char]++;
+        } else {
+            count[char] = 1;
+        }
+    }
+
+    return count;
+}
+
